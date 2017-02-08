9a004a0d90fb7e9ef2f8cc4e1e7c16c1c0da03c0
Hepolise <hepolise@gmail.com>
2017-02-08 22:26:28 +0300

    return text widget for choice

--- app/src/main/java/ru/hepolise/cellrest/TraffWidget.java
+++ app/src/main/java/ru/hepolise/cellrest/TraffWidget.java
@@ -199,6 +199,7 @@ public class TraffWidget extends AppWidgetProvider {
 
     public String updateWidget(Context context, AppWidgetManager appWidgetManager,
                                int widgetID, String content) {
+        Log.d(LOG_TAG, "content: " + content);
 
         conextglobal = context;
         appWidgetManagerglobal = appWidgetManager;
@@ -308,7 +309,7 @@ public class TraffWidget extends AppWidgetProvider {
 
         int color =  shrpr.getInt(QuickstartPreferences.color, 0xffffffff);
 
-        int text= 0;
+        int text;
         String color_text = shrpr.getString(QuickstartPreferences.color_text, "null");
         if (color_text.equals("null")){
             String hexColor = String.format("#%06X", (0xFFFFFF & color));
@@ -319,7 +320,7 @@ public class TraffWidget extends AppWidgetProvider {
                 color = text;
                 shrpr.edit().putInt(QuickstartPreferences.color, text).apply();
             } catch (IllegalArgumentException e) {
-                text = 0;
+                color = 0;
             }
         }
         //Log.d(LOG_TAG, Integer.toString(text));
@@ -400,7 +401,12 @@ public class TraffWidget extends AppWidgetProvider {
 
                 if (font.equals("n")) {
                     //Log.d(LOG_TAG, "font: normal");
-                    widgetView.setTextViewText(R.id.inet, inet);
+                    if (inet.length() < 3) {
+                        widgetView.setTextViewText(R.id.inet, "  " + inet);
+                    } else {
+                        widgetView.setTextViewText(R.id.inet, inet);
+                    }
+
                     widgetView.setTextViewText(R.id.calls, min);
                     //for center of image
                     if (maxsms.length() % 2 == 0) {
@@ -572,12 +578,12 @@ public class TraffWidget extends AppWidgetProvider {
         widgetView.setOnClickPendingIntent(R.id.text_upd_bold, pIntent);
         //for make all-widgets app in future
         //TODO
-//        //Log.d(LOG_TAG, "rebuilding widget: " + Integer.toString(widgetID));
-//        ComponentName name = new ComponentName(context, TraffWidget.class);
-//        int [] ids = appWidgetManager.getAppWidgetIds(name);
-//        for (int i: ids) {
-//            //Log.d(LOG_TAG, Integer.toString(i));
-//        }
+        //Log.d(LOG_TAG, "rebuilding widget: " + Integer.toString(widgetID));
+        ComponentName name = new ComponentName(context, TraffWidget.class);
+        int [] ids = appWidgetManager.getAppWidgetIds(name);
+        for (int i: ids) {
+            Log.d(LOG_TAG, Integer.toString(i));
+        }
         appWidgetManager.updateAppWidget(widgetID, widgetView);
         //Log.d(LOG_TAG, "widget is rebuilt...");
         return "";
@@ -595,15 +601,18 @@ public class TraffWidget extends AppWidgetProvider {
             } catch (IOException ex) {
                 updateWidget(conextglobal, appWidgetManagerglobal, id[0], "error");
             }
-            return null;
+            return Integer.toString(id[0]);
         }
 
 
 
-//        @Override
-//        protected void onPostExecute(String result) {
-//
-//        }
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(LOG_TAG, "onpostexec result: "  + result);
+            int id = Integer.parseInt(result);
+            Log.d(LOG_TAG, "onpostexec id"  + id);
+            updateWidget(conextglobal, appWidgetManagerglobal, id, "");
+        }
 
 
         private String getContent(Integer id) throws IOException {
@@ -679,7 +688,7 @@ public class TraffWidget extends AppWidgetProvider {
                     shrpr.edit().putString(QuickstartPreferences.balance, balance).apply();
 
                     updateWidget(conextglobal, appWidgetManagerglobal, id, "success");
-                    return(buffer);
+                    return("ldfyjfdlojh");
 
                 } catch (JSONException e){
 //                    JSONObject jsonObject = new JSONObject(buffer)
---
+++ app/src/main/java/ru/hepolise/cellrest/WidgetText.java
@@ -0,0 +1,320 @@
+package ru.hepolise.cellrest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.RemoteViews;
+import static android.appwidget.AppWidgetManager.ACTION_APPWIDGET_UPDATE;
+import static android.appwidget.AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT;
+import static android.appwidget.AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH;
+import static android.appwidget.AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT;
+import static android.appwidget.AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH;
+
+
+public class WidgetText extends AppWidgetProvider {
+    Context conextglobal;
+    android.appwidget.AppWidgetManager appWidgetManagerglobal;
+    int idglobal;
+    String UPD;
+    String ACTION_APPWIDGET_FORCE_UPDATE = "";
+    String login;
+    String pass;
+    String op;
+    String android_id;
+    String pin_code;
+    String locale;
+    String loc;
+    String version;
+    String token;
+    String return_;
+    String tz;
+
+
+    final String LOG_TAG = "cellLogs";
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        //Log.d(LOG_TAG, "onEnabled: ");
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+
+        for (int id : appWidgetIds) {
+            conextglobal = context;
+            appWidgetManagerglobal = appWidgetManager;
+            idglobal = id;
+            updateWidget(context, appWidgetManager, id, context.getString(R.string.updating));
+
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged (Context context,
+                                           AppWidgetManager appWidgetManager,
+                                           int appWidgetId,
+                                           Bundle newOptions) {
+        int max_h = newOptions.getInt(OPTION_APPWIDGET_MAX_HEIGHT);
+        int max_w = newOptions.getInt(OPTION_APPWIDGET_MAX_WIDTH);
+        int min_h = newOptions.getInt(OPTION_APPWIDGET_MIN_HEIGHT);
+        int min_w = newOptions.getInt(OPTION_APPWIDGET_MIN_WIDTH);
+//        Log.d(LOG_TAG, "max_h: " + max_h);
+//        Log.d(LOG_TAG, "max_w: " + max_w);
+//        Log.d(LOG_TAG, "min_h: " + min_h);
+//        Log.d(LOG_TAG, "min_w: " + min_w);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        super.onReceive(context, intent);
+        if (intent.getAction().equalsIgnoreCase(ACTION_APPWIDGET_FORCE_UPDATE)) {
+            int id = AppWidgetManager.INVALID_APPWIDGET_ID;
+            Bundle extras = intent.getExtras();
+            if (extras != null) {
+                id = extras.getInt(
+                        AppWidgetManager.EXTRA_APPWIDGET_ID,
+                        AppWidgetManager.INVALID_APPWIDGET_ID);
+
+            }
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+            Boolean setting_update = sharedPreferences.getBoolean(QuickstartPreferences.setting_update, true);
+            if (setting_update.equals(true)) {
+                UPD = "1";
+            } else {
+                UPD = "0";
+            }
+            SharedPreferences shpr = PreferenceManager.getDefaultSharedPreferences(context);
+            shpr.edit().putString(QuickstartPreferences.update, UPD).apply();
+            shpr.edit().putBoolean(QuickstartPreferences.f_update, true).apply();
+            Intent updateIntent = new Intent(context, WidgetText.class);
+            updateIntent.setAction(ACTION_APPWIDGET_UPDATE);
+            updateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,
+                    new int[] { id });
+            context.sendBroadcast(updateIntent);
+        }
+    }
+
+    public String updateWidget(Context context, AppWidgetManager appWidgetManager,
+                               int widgetID, String content) {
+        //Log.d(LOG_TAG, "upd 11");
+
+
+        SharedPreferences shrpr = PreferenceManager.getDefaultSharedPreferences(context);
+        //Load vars
+        login = shrpr.getString(QuickstartPreferences.login, "");
+        op = shrpr.getString(QuickstartPreferences.op_list, "");
+        token = shrpr.getString(QuickstartPreferences.TOKEN, "");
+        Boolean admin;
+        Boolean f_update = shrpr.getBoolean(QuickstartPreferences.f_update, false);
+
+        //in case of error loading new data
+        if (content.startsWith("error")) {
+            content = shrpr.getString(QuickstartPreferences.content, context.getString(R.string.error));
+        }
+
+        //Reformat login
+        if (login.startsWith("+7")) {
+            login = login.substring(2);
+            //Log.d(LOG_TAG, "+7 change: " + login);
+        } else if (login.startsWith("7") || login.startsWith("8")){
+            login = login.substring(1);
+            //Log.d(LOG_TAG, "7/8 change: " + login);
+        }
+
+
+        admin = login.equals("");
+
+        if (op.equals("tele2") && !admin) {
+            login = "7" + login;
+        }
+        pin_code = shrpr.getString(QuickstartPreferences.pin_code, "");
+        android_id = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        pass = shrpr.getString(QuickstartPreferences.pass, "");
+        return_ = shrpr.getString(QuickstartPreferences.return_, "calc");
+
+
+
+
+
+        if (admin) {
+            //Do not update first run if admin account
+            UPD = shrpr.getString(QuickstartPreferences.update, "0");
+        } else {
+            UPD = shrpr.getString(QuickstartPreferences.update, "1");
+        }
+
+        //Load location variable
+        loc = shrpr.getString(QuickstartPreferences.loc, "def");
+        Locale currentLocale = Locale.getDefault();
+        locale = currentLocale.toString();
+        if (loc.equals("def")) {
+            loc = locale;
+        }
+
+        //Load timezone
+        tz = TimeZone.getDefault().getID();
+        //Log.d(LOG_TAG, tz);
+
+        //loading app version
+        try {
+            version = context.getPackageManager().getPackageInfo(context.getPackageName(), 0 ).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+
+        }
+        //int versionCode = BuildConfig.VERSION_CODE;
+
+        if (content.equals(context.getString(R.string.updating))) {
+            if (f_update) {
+                shrpr.edit().putBoolean(QuickstartPreferences.f_update, false).apply();
+            } else {
+                content = shrpr.getString(QuickstartPreferences.content, context.getString(R.string.error));
+            }
+            //content = context.getString(R.string.updating);
+            //Starting to load content
+            new ProgressTask().execute();
+            if (admin) {
+                // in case of admin's account
+                if (UPD.equals("1")) {
+                    shrpr.edit().putString(QuickstartPreferences.update, "0").apply();
+                }
+            } else {
+                shrpr.edit().putString(QuickstartPreferences.update, "1").apply();
+            }
+        } else {
+            //Save new content
+            shrpr.edit().putString(QuickstartPreferences.content, content).apply();
+        }
+
+
+
+        // set Widget view
+
+        RemoteViews widgetView = new RemoteViews(context.getPackageName(),
+                R.layout.widget_text);
+        //Working with colors
+        int color =  shrpr.getInt(QuickstartPreferences.color, 0xffffffff);
+
+
+        //Setting warning about admin's account
+//        if (admin && (!content.equals(context.getString(R.string.updating)))){
+//            widgetView.setTextViewText(R.id.text_default, context.getString(R.string.admin_acc));
+//            widgetView.setTextColor(R.id.text_default, color);
+//        } else {
+//            widgetView.setTextViewText(R.id.text_default, "");
+//        }
+
+        //Set all text to null
+        widgetView.setTextViewText(R.id.text_light, "");
+        widgetView.setTextViewText(R.id.text_bold, "");
+        widgetView.setTextViewText(R.id.text_italic, "");
+        int res = R.id.text_light;
+        //Changing text type
+        String font =  shrpr.getString(QuickstartPreferences.font, "n");
+        if (font.equals("i")) {
+            res = R.id.text_italic;
+        } else if(font.equals("b")) {
+            res = R.id.text_bold;
+        }
+
+
+        //Setting content to widget and updating it
+        widgetView.setTextViewText(res, content);
+        widgetView.setTextColor(res, color);
+        Intent updateIntent = new Intent(context, WidgetText.class);
+        updateIntent.setAction(ACTION_APPWIDGET_FORCE_UPDATE);
+
+        updateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
+        android.app.PendingIntent pIntent = PendingIntent.getBroadcast(context, widgetID, updateIntent, 0);
+        widgetView.setOnClickPendingIntent(res, pIntent);
+        appWidgetManager.updateAppWidget(widgetID, widgetView);
+        return (null);
+    }
+
+    class ProgressTask extends AsyncTask<String, Void, String> {
+        @Override
+        public String doInBackground(String... path) {
+
+            try {
+                //Loading content
+                getContent();
+            } catch (IOException ex) {
+                updateWidget(conextglobal, appWidgetManagerglobal, idglobal, "error");
+            }
+            return null;
+        }
+
+
+        private String getContent() throws IOException {
+            BufferedReader reader;
+
+            try {
+
+                URL url = new URL("https://srvr.tk/traf.php?cmd=widget&upd=" + URLEncoder.encode(UPD, "UTF-8") +
+                        "&login=" + URLEncoder.encode(login, "UTF-8") +
+                        "&pass=" + URLEncoder.encode(pass, "UTF-8") +
+                        "&op=" + URLEncoder.encode(op, "UTF-8") +
+                        "&devid=" + URLEncoder.encode(android_id, "UTF-8") +
+                        "&pin=" + URLEncoder.encode(pin_code, "UTF-8") +
+                        "&loc=" + URLEncoder.encode(loc, "UTF-8") +
+                        "&version=" + URLEncoder.encode(version, "UTF-8") +
+                        "&token=" + URLEncoder.encode(token, "UTF-8") +
+                        "&return=" + URLEncoder.encode(return_, "UTF-8") +
+                        "&tz=" + URLEncoder.encode(tz, "UTF-8")
+                        //+ "&test"
+                );
+                //Log.d(LOG_TAG, "URL: " + url);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                reader= new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder buf=new StringBuilder();
+                String line;
+                while ((line=reader.readLine()) != null) {
+                    buf.append(line + " ");
+                }
+                String buffer = buf.toString();
+                buffer = buffer.replace(" NEWLINE ", "\n");
+                updateWidget(conextglobal, appWidgetManagerglobal, idglobal, buffer);
+                return(buffer);
+
+            } catch (IOException e) {
+                //Log.d(LOG_TAG, e.getMessage());
+                updateWidget(conextglobal, appWidgetManagerglobal, idglobal, "error");
+                return e.getMessage();
+            }
+
+        }
+
+    }
+}
