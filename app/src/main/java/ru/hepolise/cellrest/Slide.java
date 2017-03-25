3745e8deee35250eec2cde9e5e3e5c664b26911b
Hepolise <admin@srvr.tk>
2017-03-25 20:34:07 +0300

    add files

---
+++ app/src/main/java/ru/hepolise/cellrest/Slide.java
@@ -0,0 +1,44 @@
+package ru.hepolise.cellrest;
+
+/**
+ * Created by Hepolise on 25.03.2017.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Slide extends Fragment {
+
+    private static final String ARG_LAYOUT_RES_ID = "layoutResId";
+    private int layoutResId;
+
+    public static Slide newInstance(int layoutResId) {
+        Slide Slide = new Slide();
+
+        Bundle args = new Bundle();
+        args.putInt(ARG_LAYOUT_RES_ID, layoutResId);
+        Slide.setArguments(args);
+
+        return Slide;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null && getArguments().containsKey(ARG_LAYOUT_RES_ID)) {
+            layoutResId = getArguments().getInt(ARG_LAYOUT_RES_ID);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(layoutResId, container, false);
+    }
+}
