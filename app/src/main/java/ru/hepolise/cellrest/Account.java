b1339fe7b4795f83ac84e16e07004d88a7e2b6e8
Hepolise <hepolise@gmail.com>
2017-04-24 22:13:23 +0300

    work with profiles (2)

--- app/src/main/java/ru/hepolise/cellrest/Account.java
+++ app/src/main/java/ru/hepolise/cellrest/Account.java
@@ -1,8 +1,10 @@
 package ru.hepolise.cellrest;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -27,8 +29,9 @@ public class Account extends ListActivity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_account);
-        String[] values = new String[] { "+79672028860", "54564654gvj" };
+        String[] values = new String[] { "null", "first", "second" };
 
+        final int len = values.length - 1;
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
@@ -36,20 +39,24 @@ public class Account extends ListActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences myPrefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
+                SharedPreferences myPrefs = getSharedPreferences("prefs_" + Integer.toString(len), MODE_PRIVATE);
                 SharedPreferences.Editor prefsEditor;
                 prefsEditor = myPrefs.edit();
 //strVersionName->Any value to be stored
-                prefsEditor.putString("STOREDVALUE", "123");
+                prefsEditor.putString("CHECKVALUE", "HEH");
                 prefsEditor.commit();
+                SharedPreferences shrpr = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                shrpr.edit().putString("wokring_prefs", "prefs_" + Integer.toString(len)).commit();
             }
         });
     }
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-        String item = (String) getListAdapter().getItem(position);
-        Toast.makeText(this, item + " выбран", Toast.LENGTH_LONG).show();
+        //String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, Integer.toString(position) + " выбран", Toast.LENGTH_LONG).show();
+        SharedPreferences shrpr = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        shrpr.edit().putString("wokring_prefs", "prefs_" + Integer.toString(position)).commit();
     }
 }
 
