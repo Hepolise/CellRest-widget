c42f467d1ea7321edd2edf93d313d952cde0329d
Hepolise <hepolise@gmail.com>
2018-02-16 22:36:50 +0300

    Bugfixes

--- app/src/main/java/ru/hepolise/cellrest/Activities/HelpActivity.java
+++ app/src/main/java/ru/hepolise/cellrest/Activities/HelpActivity.java
@@ -16,9 +16,9 @@ import android.widget.TextView;
 
 import java.util.Locale;
 
+import ru.hepolise.cellrest.BuildConfig;
 import ru.hepolise.cellrest.R;
 
-import static android.R.attr.versionCode;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -40,12 +40,14 @@ public class HelpActivity extends AppCompatActivity {
 //        } catch (PackageManager.NameNotFoundException e) {
 //
 //        }
+        int versionCode = BuildConfig.VERSION_CODE;
         version = Integer.toString(versionCode);
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
         // Enable Javascript
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         //Log.d("traff: ", locale);
+        Log.d("cellL", ("https://srvr.su/traffic/help.php?l=" + locale + "&ver=" + version));
         mWebView.loadUrl("https://srvr.su/traffic/help.php?l=" + locale + "&v=" + version);
         mWebView.setDownloadListener(new DownloadListener() {
             public void onDownloadStart(String url, String userAgent,
